63e94e74bd9434a514490a599403f4c570ca431b
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-01-08 21:03:53 +0800

    updated spaces

--- app/app/test.py
+++ app/app/test.py
@@ -11,8 +11,8 @@ class CalcTest(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
+    
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
