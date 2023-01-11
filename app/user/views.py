ecf537ba855d6f5c8e273dbba557df0f2541f2e9
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-01-11 14:09:15 +0800

    updated codebase with user serializers and user tests

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+"""views for user API"""
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
