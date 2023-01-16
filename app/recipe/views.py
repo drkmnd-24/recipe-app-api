794e9c64805e06db7ab3e1f9a5d2f5212b2cac4e
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-01-16 22:31:37 +0800

    expected 2 blanks error fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -16,6 +16,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe, Tag, Ingredient
 from . import serializers
 
+
 @extend_schema_view(
     list=extend_schema(
         parameters=[
